86069ec13ddf086f1a21cc85c4889f384af16863
JenisBen <benjamin199551@gmail.com>
2022-11-10 08:26:41 -0800

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,12 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { UseWalletProvider } from "use-wallet2";
+
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+    return (
+        <UseWalletProvider autoConnect={true}>
+            <Component {...pageProps} />
+        </UseWalletProvider>
+    );
 }
---
+++ pages/about/index.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function About() {
+    return (
+        <div>
+            <img src={"./assets/back.jpg"} alt="" />
+            <h1>About</h1>
+        </div>
+    );
+}
